cbb286b9143c70927cdb879eb896e3eadf5ff320
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-05-28 16:24:25 +0200

    added comments

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// to jest okienko alert
+alert('ALERT - czyli tylko OK')
+// to jest okienku confirm
+confirm('CONFIRM, czyli OK i ANULUJ')
+
+
+/*nie rozumiem, co nam daje ta funkcja 
+console.log('co daje ta funkcja')
+*/
